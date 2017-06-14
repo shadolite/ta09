48124859225f9184458fba552535db8f598c3661
shadolite <amy.kawaii@gmail.com>
2017-06-14 17:43:46 -0600

    initial file

---
+++ ta09.cpp
@@ -0,0 +1,46 @@
+/***********************************************************************
+* Program:
+*    Team Activity 09, Polymorphism   
+*    Brother Alvey, CS165
+* Summary: 
+*    Summaries are not necessary for checkpoint assignments.
+* ***********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+/**********************************************************************
+* Class: Employee
+* Purpose: 
+***********************************************************************/
+class Employee
+{
+
+};
+
+/**********************************************************************
+ * Class: HourlyEmployee
+ * Purpose:
+ ***********************************************************************/
+class HourlyEmployee : public Employee
+{
+
+};
+
+/**********************************************************************
+ * Class: SalaryEmployee
+ * Purpose:
+ ***********************************************************************/
+class SalaryEmployee : public Employee
+{
+
+};
+
+/**********************************************************************
+* Function: main
+* Purpose: This is the entry point and driver for the program.
+***********************************************************************/
+int main()
+{
+   return 0;
+}
